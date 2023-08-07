bb4ea5bb100f1dd7be4ddcd444c0bcc1d165ef37
EminAliyev93 <eminemin99335@gmail.com>
2023-08-07 14:37:37 +0400

    First proje

---
+++ MyFirstJavaApplication/src/myfirstjavaapplication/Main.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myfirstjavaapplication;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Emin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Please enter number manth");
+            int manth = sc.nextInt();
+
+            switch (manth) {
+                case 1:
+                    System.out.println("JANUARY");
+                    break;
+                case 2:
+                    System.out.println("FEBRUARY");
+                    break;
+                case 3:
+                    System.out.println("MARCH");
+                    break;
+                case 4:
+                    System.out.println("APRIL");
+                    break;
+                case 5:
+                    System.out.println("MAY");
+                    break;
+                case 6:
+                    System.out.println("JUNE");
+                    break;
+                case 7:
+                    System.out.println("JULY");
+                    break;
+                case 8:
+                    System.out.println("AUGUST");
+                    break;
+                case 9:
+                    System.out.println("SEPTEMBER");
+                    break;
+                case 10:
+                    System.out.println("OCTOBER");
+                    break;
+                case 11:
+                    System.out.println("NOVEMBER");
+                    break;
+                case 12:
+                    System.out.println("DECEMBER");
+                    break;
+                default:
+                    System.out.println("PLEASE ENTER CORRECT NUMBER");
+                    System.out.println("---------------------------");
+            }
+        }
+    }
+
+}
+    
+
